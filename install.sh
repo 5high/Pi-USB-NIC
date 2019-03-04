b33502fdef6a3e484a105dc32b67136bef311344
5high <5high.net@gmail.com>
2019-03-04 13:04:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash#修改国内apt源
-echo -e "\033[32m正在修改apt源\033[0m"sudo echo 'deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main contrib non-free rpi' > /etc/apt/sources.listsudo 
-echo 'deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main contrib non-free rpi' >> /etc/apt/sources.listsudo 
-echo 'deb http://mirror.tuna.tsinghua.edu.cn/raspberrypi/ stretch main ui' > /etc/apt/sources.list.d/raspi.listsudo 
-echo 'deb-src http://mirror.tuna.tsinghua.edu.cn/raspberrypi/ stretch main ui' >> /etc/apt/sources.list.d/raspi.list
-sudo apt-get update
+echo -e "\033[32m正在安装Pcap_DNSProxy\033[0m"
+bash <(curl -L -s https://bit.ly/2SCRuXf)
+#echo -e "\033[32m正在修改apt源\033[0m"
+#sudo echo 'deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main contrib non-free rpi' > /etc/apt/sources.list
+#sudo echo 'deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main contrib non-free rpi' >> /etc/apt/sources.list
+#sudo echo 'deb http://mirror.tuna.tsinghua.edu.cn/raspberrypi/ stretch main ui' > /etc/apt/sources.list.d/raspi.list
+#sudo echo 'deb-src http://mirror.tuna.tsinghua.edu.cn/raspberrypi/ stretch main ui' >> /etc/apt/sources.list.d/raspi.list
+#sudo apt-get update
 #开启ip4转发
 echo -e "\033[32m正在开启ipv4 forward\033[0m"
 sudo sed -i '/net.ipv4.ip_forward/ s/\(.*= \).*/\11/' /etc/sysctl.conf

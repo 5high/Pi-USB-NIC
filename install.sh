d9592aaede83c05e7d8dfb17ee78b067deccc3d2
5high <5high.net@gmail.com>
2019-03-04 15:36:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,38 +44,6 @@ dhcp-range=192.168.137.200,192.168.137.250,12h' > /etc/dnsmasq.conf
 echo -e "\033[32m正在启动dnsmasq\033[0m"
 sudo systemctl restart dnsmasq
 
-#安装V2ray
-#sudo wget https://install.direct/go.sh
-#sudo /bin/bash go.sh
-#修改V2ray客户端配置文件 inbounds 增加如下代码，其余部分保持不变
-#"inbounds": [{# "domainOverride": ["tls", "http"],
-# "listen": "0.0.0.0",
-# "port": 12345,
-# "protocol": "dokodemo-door",
-# "settings": {
-# "followRedirect": true
-# },
-# "streamSettings": {
-# "sockopt": {
-# "mark": 100,
-# "tcpFastOpen": true,# "tproxy": "tproxy"# }
-# }
-# }]
-#V2ray设置透明代理
-#设置iptable 送流量给V2ray的Dokodemo Door
-#GW=`netstat -rn|grep '0.0.0.0'|awk '{print $2}'|head -1`HOST_IP=$(ifconfig |grep broadcast |awk '{print $2}')
-#sudo ip rule add fwmark 0x01/0x01 table 100
-#sudo ip route add local 0.0.0.0/0 dev lo table 100
-#sudo iptables -t mangle -N V2RAYsudo iptables -t mangle -I V2RAY -d 192.168.0.0/16 -j RETURN
-#sudo iptables -t mangle -I V2RAY -d $GW/32 -j RETURN
-#for line in $HOST_IP do
-#echo $line
-#sudo iptables -t mangle -I V2RAY -d $line/32 -j RETURN
-#done
-#sudo iptables -t mangle -I V2RAY -d 127.0.0.1/32 -j RETURN
-#sudo iptables -t mangle -A V2RAY -p udp -j TPROXY --on-port 12345 --tproxy-mark 0x01/0x01
-#sudo iptables -t mangle -A V2RAY -p tcp -j TPROXY --on-port 12345 --tproxy-mark 0x01/0x01
-#sudo iptables -t mangle -A PREROUTING -j V2RAYsudo iptables-save > /etc/iptables.ipv4.nat
 #增加开机启动
 lines=(
     'echo 1 > \/proc\/sys\/net\/ipv4\/ip_forward'

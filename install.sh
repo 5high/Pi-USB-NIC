b8d066deab48026ee9704226ca260a4214ea2d71
5high <5high.net@gmail.com>
2019-04-01 09:32:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ bash <(curl -L -s https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/inst
 bash <(curl -L -s https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/install-without-dns.sh)
 bash <(curl -L -s https://install.direct/go.sh)
 cd /etc
+wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/rc.local

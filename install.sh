d0ce5859f171822d79bbfd354815c9da10bd3b00
5high <5high.net@gmail.com>
2019-04-01 09:45:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd $HOME
 wget -N https://raw.githubusercontent.com/5high/Pcap_DNSProxy/master/install.sh
+chmod +x install.sh
 bash install.sh
 wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/install-without-dns.sh
+chmod +x install-without-dns.sh
 bash install-without-dns.sh
 wget -N https://install.direct/go.sh
+chmod +x go.sh
 bash go.sh
 cd /etc
 wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/rc.local

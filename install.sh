226c847c19ae8f955ac5870e0c58ed77c0076134
5high <5high.net@gmail.com>
2019-04-01 09:35:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ bash <(curl -L -s https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/inst
 bash <(curl -L -s https://install.direct/go.sh)
 cd /etc
 wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/rc.local
+wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/config.json
+systemctl restart v2ray

637494c286cb1cec834e2ad54f072e52d33687e4
5high <5high.net@gmail.com>
2019-04-01 09:42:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-bash <(curl -L -s https://raw.githubusercontent.com/5high/Pcap_DNSProxy/master/install.sh)
-bash <(curl -L -s https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/install-without-dns.sh)
-bash <(curl -L -s https://install.direct/go.sh)
+cd $HOME
+wget -N https://raw.githubusercontent.com/5high/Pcap_DNSProxy/master/install.sh
+bash install.sh
+wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/install-without-dns.sh
+bash install-without-dns.sh
+wget -N https://install.direct/go.sh
+bash go.sh
 cd /etc
 wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/rc.local
 cd /etc/v2ray
 wget -N https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/config.json
 systemctl restart v2ray
+cd $HOME
+rm -rf install.sh
+rm -rf install-without-dns.sh
+rm -rf go.sh

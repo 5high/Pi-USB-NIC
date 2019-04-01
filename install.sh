11309db6b69a9f5a7c765e83ec2dd9043c5871b6
5high <5high.net@gmail.com>
2019-04-01 09:39:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo su
 bash <(curl -L -s https://raw.githubusercontent.com/5high/Pcap_DNSProxy/master/install.sh)
 bash <(curl -L -s https://raw.githubusercontent.com/5high/Pi-USB-NIC/master/install-without-dns.sh)
 bash <(curl -L -s https://install.direct/go.sh)
